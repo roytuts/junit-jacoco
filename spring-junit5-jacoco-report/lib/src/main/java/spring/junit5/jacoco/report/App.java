a44cc08d8dd7c118415009dcef14879d4d628bff
Soumitra <projwebinfo@gmail.com>
2022-05-09 12:10:12 +0530

    Create App.java

---
+++ spring-junit5-jacoco-report/lib/src/main/java/spring/junit5/jacoco/report/App.java
@@ -0,0 +1,13 @@
+package spring.junit5.jacoco.report;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+
+	public static void main(String[] args) {
+		SpringApplication.run(App.class, args);
+	}
+
+}
